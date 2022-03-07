c8f0bbe39ace5159c7db0bd569c9df3dbc595f37
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2022-03-07 17:09:10 +0100

    Get predictions (y_hat) Using Predictor

---
+++ Lindel_prediction.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python 
+# Author: Will Chen
+''' 
+1. All functions are tested under python3.5 and python 3.6
+2. Add Lindel folder to your python path.
+3. y_hat is the prediction of all ~450 classes of indels <30bp.
+4. fs is the frameshift ratio for this sequence.
+5. Input should be 65bp (30 bp upstream and 35 bp downstream of the cleavage site)
+usage: pyton Lindel_predction.py your_sequence_here your_file_name_here(can be gene name or guide name you designed)
+'''
+import Lindel, os, sys
+from Lindel.Predictor import * 
+import pickle as pkl
+import pandas as pd
+
+model_weights = pkl.load(open(os.path.join("./Lindel/Given_Model_Weights.pkl"),'rb'))
+
+# label: dict where keys are the outcome classes and the values are the corresponding indices of columns
+# rev_index: dict where the keys are the indices of columns and the values are the corresponding outcome classes
+# features: dict where the keys are the features and the values are the indices
+label, rev_index, features = pkl.load(open(os.path.join('./data/feature_index_all.pkl'),'rb'))
+
+# Load in Data Matrix
+data = pd.read_csv("./data/Lindel_test.txt", sep='\t')
+
+# Generate predictions for each sequence
+for index, entry in data.iterrows():
+
+    # Guide sequence
+    seq = str(entry[0])
+    features = entry[1:3034].values.astype(np.float64)
+    labels = entry[3034:].values.astype(np.float64)
+
+    y_hat = gen_prediction(seq, features, model_weights)
+
+    # Get predictions for each of the 557 classes (probabilities sum up to 1)
+    pred_freq = {}
+    for i in range(len(y_hat)):
+        if y_hat[i]!=0:
+            pred_freq[rev_index[i]] = y_hat[i]
+    
+    # Sort predictions in descending order
+    pred_sorted = sorted(pred_freq.items(), key=lambda kv: kv[1],reverse=True)
+
+    # TODO: Do something with your predictions!
